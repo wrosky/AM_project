5a0711effb69ec87e5dcd0181f5ec045e04ec2bd
wrosky <a.wronski12345@gmail.com>
2023-10-22 22:05:47 +0200

    9th Commit

--- base/views.py
+++ base/views.py
@@ -88,6 +88,11 @@ def room(request, pk):
     context = {'room': room, 'room_messages': room_messages, 'participants': participants}
     return render(request, 'base/room.html', context)
 
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    context = {'user': user}
+    return render(request, 'base/profile.html', context)
+
 @login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
