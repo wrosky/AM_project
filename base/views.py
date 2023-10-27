143b31e45f7d2ce52729ea1f9510de66cfb2233a
wrosky <a.wronski12345@gmail.com>
2023-10-27 20:33:39 +0200

    10th Commit

--- base/views.py
+++ base/views.py
@@ -90,7 +90,10 @@ def room(request, pk):
 
 def userProfile(request, pk):
     user = User.objects.get(id=pk)
-    context = {'user': user}
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {'user': user, 'rooms': rooms, 'room_messages': room_messages, 'topics': topics}
     return render(request, 'base/profile.html', context)
 
 @login_required(login_url='login')
@@ -99,7 +102,9 @@ def createRoom(request):
     if request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
-            form.save()
+            room = form.save(commit=False)
+            room.host = request.user
+            room.save()
             return redirect('home')
 
     context = {'form': form}
